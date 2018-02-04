4dd7061521486b3f8275d434b3e97df904d9ccaa
FallenLight <xFallenLight@users.noreply.github.com>
2018-02-04 14:42:26 +0800

    Declared and defined an fs var for Lunar to use.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const fs = require("fs");
 
 client.on('ready', () => {
   console.log(`I'm ready! (Logged in as: ${client.user.tag})`);
