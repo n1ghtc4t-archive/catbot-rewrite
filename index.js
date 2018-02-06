b489fbe2e330410f1f52a43baf3ceb8dc01473a0
Thief (goddy) <onememeonly@gmail.com>
2018-02-06 15:56:40 +0800

    fix userdata var

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
 
-const USER_DATA_FILE = "userdata.json";
+const USER_DATA_FILE = require("./userdata.json");
 
 client.on('ready', () => {
   console.log(`I'm ready! (Logged in as: ${client.user.tag})`);
