6f7287d2ee10381d1bc6d574b89a460704fcf415
Sodre177 <adam@adamwalsh.name>
2018-02-06 18:50:21 +1000

    \!fixup Experimental userdata and rep. Testing

--- index.js
+++ index.js
@@ -160,7 +160,8 @@ client.on('message', msg => {
 	} */
 	if (msg.content.startsWith(prefix + 'rep')) {
 		var user = msg.mentions.members.first();
-		!user ? return msg.reply("Please provide a user mention!") : userid = user.id;
+		if(!user) return msg.reply("Please provide a user mention!") ;
+		userid = user.id;
 		if (userid == msg.author.id) return msg.reply("You can't give reputation to yourself!");
 
 		usersdata = UserData[userid] ? UserData[userid] : {};
