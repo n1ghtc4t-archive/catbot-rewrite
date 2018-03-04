07084ecdc2defeea08f38e1e8ed9248c18912481
FallenLight <xFallenLight@users.noreply.github.com>
2018-03-04 11:38:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -322,7 +322,7 @@ for (cmd in cmds) {
 //Start the client and get going!
 client.on('ready', () => {
 	console.log(`I'm ready! (Logged in as: ${client.user.tag})`);
-	client.user.setActivity(PREFIX+'help for help!', {type: 'STREAMING'});
+	client.user.setActivity(PREFIX+'help for help!', {type: 'STREAMING', url: 'https://www.twitch.tv/goddycodes'});
 	// https://www.twitch.tv/goddycodes
 });
 
